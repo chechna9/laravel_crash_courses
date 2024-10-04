1d201575a804aad0f92b32a278ee460e680f69d7
chechna9 <ja_bouchouareb@esi.dz>
2024-10-04 15:38:17 +0100

    models, migration tables, factories

---
+++ database/factories/InvoiceFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Invoice>
+ */
+class InvoiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $status = $this->faker->randomElement(['B','P','V']);
+
+        return [
+            'customer_id' => CustomerFactory::factory(),
+            'amount' => $this->faker->numberBetween(100,20000),
+            'status' => $status,
+            'billed_date' => $this->faker->dateTimeThisDecade(),
+            'paid_date' => $status == 'P' ? $this->faker->dateTimeThisDecade() : null,
+            
+        ];
+    }
+}
---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Customer;
+use Database\Factories\CustomerFactory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        Customer::factory()
+        ->count(25)
+        ->hasInovices(10)
+        ->create();
+    }
+}
