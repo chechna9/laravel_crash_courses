262dac51dc560ee5d82d8a91668901267fd94e02
chechna9 <ja_bouchouareb@esi.dz>
2024-10-08 02:03:17 +0100

    created customerResource for mapping the json on show method

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -6,6 +6,7 @@ use App\Models\Customer;
 use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\CustomerResource;
 class CustomerController extends Controller
 {
     /**
@@ -37,7 +38,7 @@ class CustomerController extends Controller
      */
     public function show(Customer $customer)
     {
-        //
+        return new CustomerResource($customer);
     }
 
     /**
